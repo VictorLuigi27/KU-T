156fd19eb0e7439711cada4e6e7508f123185e3a
VictorLuigi27 <victor.diaz@oclock.school>
2026-02-24 14:19:45 +0100

    Page d'accueil position

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-start p-8 bg-black">
-        <div className='ml-20'>
-                <h1 className="text-[8rem] font-light text-white mb-20">
-                    Victor Kurt
-                </h1>
+      <div className="ml-20">
+        <h1 className="text-[8rem] font-light text-white mb-20">
+          Victor Kurt
+        </h1>
 
-            <div className='space-y-10'>
-                <p className="text-4xl text-gray-300">
-                    Mon parcours
-                </p>
+        <div className="space-y-10 flex flex-col">
+          <Link to="/parcours" className="text-4xl text-gray-300 hover:text-white hover:translate-x-5 transition-transform duration-300">
+            Mon parcours
+          </Link>
 
-                <p className="text-4xl text-gray-300">
-                    Mes Atouts
-                </p>
+          <Link to="/atouts" className="text-4xl text-gray-300 hover:text-white hover:translate-x-5 transition-transform duration-300">
+            Mes Atouts
+          </Link>
 
-                <p className="text-4xl text-gray-300">
-                    Mes Projets
-                </p>
+          <Link to="/projets" className="text-4xl text-gray-300 hover:text-white hover:translate-x-5 transition-transform duration-300">
+            Mes Projets
+          </Link>
 
-                <p className="text-4xl text-gray-300">
-                    Contact
-                </p>
-            </div>
+          <Link to="/contact" className="text-4xl text-gray-300 hover:text-white hover:translate-x-5 transition-transform duration-300">
+            Contact
+          </Link>
         </div>
+      </div>
     </div>
   );
 };
